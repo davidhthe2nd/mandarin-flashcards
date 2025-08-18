582ac37c41be52ab7e6fbce15f24f6ffc1c7e4e4
davidhthe2nd <florezherrero@gmail.com>
2025-08-18 16:43:38 +0200

    Implementing Flutterflow

--- mandarin_flashcards/lib/state/options_state.dart
+++ mandarin_flashcards/lib/state/options_state.dart
@@ -4,10 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'hive_keys.dart';
 
 class OptionsState extends ChangeNotifier {
-  OptionsState(this._optionsBoxName) {
-    _box = Hive.box(_optionsBoxName);
-    _read();
-  }
+  OptionsState(this._optionsBoxName);
 
   final String _optionsBoxName;
   late final Box _box;
@@ -15,30 +12,53 @@ class OptionsState extends ChangeNotifier {
   bool _showPinyin = true;
   bool _invertPair = false;
   bool _darkMode = false; // 🌙 new
+  int _dailyTarget = 20;
 
   bool get showPinyin => _showPinyin;
   bool get invertPair => _invertPair;
   bool get darkMode => _darkMode; // 🌙 new
+  int get dailyTarget => _dailyTarget;
 
-  void _read() {
-    final map = Map<String, dynamic>.from(
-      _box.get(kOptionsPrefsKey, defaultValue: const {
-        'showPinyin': true,
-        'invertPair': false,
-        'darkMode': false, // 🌙 default
-      }),
-    );
-    _showPinyin = (map['showPinyin'] as bool?) ?? true;
-    _invertPair = (map['invertPair'] as bool?) ?? false;
-    _darkMode   = (map['darkMode']   as bool?) ?? false; // 🌙
+  /// Initialize the box and load options.
+  Future<void> init() async {
+    _box = await Hive.openBox(_optionsBoxName);
+    await _read();
   }
 
-  void _write() {
-    _box.put(kOptionsPrefsKey, {
+  Future<void> _read() async {
+    final raw = _box.get(kOptionsPrefsKey);
+
+    Map<String, dynamic> map;
+    if (raw is Map) {
+      map = Map<String, dynamic>.from(raw);
+    } else {
+      map = <String, dynamic>{};
+    }
+
+    _showPinyin = (map['showPinyin'] as bool?) ?? true;
+    _invertPair = (map['invertPair'] as bool?) ?? false;
+    _darkMode   = (map['darkMode']   as bool?) ?? false;
+
+    final dt = map['dailyTarget'];
+    if (dt is num) {
+      _dailyTarget = dt.toInt().clamp(1, 200);
+    } else if (dt is String) {
+      _dailyTarget = int.tryParse(dt)?.clamp(1, 200) ?? 20;
+    } else {
+      _dailyTarget = 20;
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> _write() async {
+    final data = <String, dynamic>{
       'showPinyin': _showPinyin,
       'invertPair': _invertPair,
-      'darkMode': _darkMode, // 🌙
-    });
+      'darkMode': _darkMode,
+      'dailyTarget': _dailyTarget,
+    };
+    await _box.put(kOptionsPrefsKey, data);
   }
 
   void toggleShowPinyin(bool value) {
@@ -53,9 +73,15 @@ class OptionsState extends ChangeNotifier {
     notifyListeners();
   }
 
-  void toggleDarkMode(bool value) { // 🌙 new
+  void toggleDarkMode(bool value) {
     _darkMode = value;
     _write();
+    notifyListeners();
+  }
+
+  Future<void> setDailyTarget(int value) async {
+    _dailyTarget = value.clamp(1, 200);
+    await _write();
     notifyListeners();
   }
 }
